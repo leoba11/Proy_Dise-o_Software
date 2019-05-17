47c77085a9e1a5e51d57cc7aabcdf25cb9cbbb1f
Josué Zeledón <josue.zeledon0727@gmail.com>
2019-05-17 15:04:01 -0600

    Window finished

--- Chess/Ventana.py
+++ Chess/Ventana.py
@@ -1,44 +1,57 @@
 from tkinter import *
 
-
 class VentanaInicial(Frame):
-
-	# Builder of the class
+	
+    # Variables to store the players names
+    playerWhite_name = "" 
+    playerBlack_name = ""
+	
+    # Builder of the class
     def __init__(self, master):
         super().__init__()
-
-        self.initUI(master)
-
-	# Subrotuine that "builds" all the labels and boxes needed to 
-	# load the player's name
-    def initUI(self, master):
-
+        
         self.master.title("Ajedrez Diseno de Software")
         self.pack()
 
-		# Creates the first label with the Entry box for the white pieces player
-        lbl1 = Label(master, text="Jugador con fichas blancas:", width=25, anchor=NW, fg="white",font=("Helvetica", 16))
+		# Creates the labels with the Entry boxes for the white and black pieces players
+        self.lbl1 = Label(master, text="Jugador con fichas blancas:", width=25, anchor=NW, fg="white",font=("Helvetica", 16))
         
         # places the object in a relative position of the window
-        lbl1.place(relx=0.08, rely=0.2)
+        self.lbl1.place(relx=0.08, rely=0.2)
 
-        entry1 = Entry(master, width=20, bg="white", justify = CENTER, font=("Helvetica", 16))
-        entry1.place(relx=0.55, rely=0.2)
+        self.entry1 = Entry(master, width=20, bg="white", justify = CENTER, font=("Helvetica", 16))
+        self.entry1.place(relx=0.55, rely=0.2)
 
-        lbl2 = Label(master, text="Jugador con fichas negras:", width=25, anchor=NW, font=("Helvetica", 16))
-        lbl2.place(relx=0.08, rely=0.5)
+        self.lbl2 = Label(master, text="Jugador con fichas negras:", width=25, anchor=NW, font=("Helvetica", 16))
+        self.lbl2.place(relx=0.08, rely=0.5)
 
-        entry2 = Entry(master, width=20, bg="white", justify = CENTER, font=("Helvetica", 16))
-        entry2.place(relx=0.55, rely=0.5)
+        self.entry2 = Entry(master, width=20, bg="white", justify = CENTER, font=("Helvetica", 16))
+        self.entry2.place(relx=0.55, rely=0.5)
         
-        buttonPlay = Button(master, text="Jugar!!", bg="green", bd=5, justify=CENTER, width=10, font=("Helvetica", 16))
-        buttonPlay.place(relx=0.65, rely=0.75)
+        # Creates a text box
+        self.box = Text(master, width=45, height=4, wrap=WORD, background="white")
+        self.box.place(relx=0.10, rely=0.75)
+		
+        # Creates the button to submit the players names
+        self.buttonPlay = Button(master, text="Jugar!!", bg="green", bd=5, justify=CENTER, width=10, font=("Helvetica", 16), command=self.button_click)
+        self.buttonPlay.place(relx=0.65, rely=0.75)
+        
+    # Subroutine that extracts the players names and stores them into the class attributes 
+    def button_click(self):
+        playerWhite_name = self.entry1.get()
+        playerBlack_name = self.entry2.get()
+        self.box.delete(0.0, END)
+        self.box.insert(END, playerWhite_name + " juega con fichas blancas")
+        self.box.insert(END, " y ")
+        self.box.insert(END, playerBlack_name + " juega con fichas negras")
+        #print(playerWhite_name)
+        #print(playerBlack_name)
 
 def main():
 
     root = Tk()
-    root.geometry("600x300+650+300")
-    app = VentanaInicial(root).pack()
+    root.geometry("600x300")
+    app = VentanaInicial(root)#.pack()
     root.mainloop()
 
 if __name__ == '__main__':
