b64f0f70bcc515b00648437a7d4d3985e04350a3
Berta <nosarabs@gmail.com>
2019-05-20 17:18:29 -0600

    Update

--- Chess/VentanaGuardar.py
+++ Chess/VentanaGuardar.py
@@ -12,18 +12,21 @@ class VentanaGuardar(Frame):
         self.pack()
 
         # Creates the button to submit the players names
-        self.buttonLoad = Button(master, text="Cargar Partida", bg="green", bd=5, justify=CENTER, width=20, font=("Helvetica", 16), command=self.button_load)
-        self.buttonLoad.place(relx=0.65, rely=0.75)
+        self.buttonLoad = Button(master, text="Cargar Partida", bg="green", bd=5, justify=CENTER, width=15, font=("Helvetica", 16), command=self.button_load)
+        self.buttonLoad.place(relx=0.65, rely=0.40)
 
-        self.buttonSave = Button(master, text="Guardar Partida", bg="blue", bd=5, justify=CENTER, width=20, font=("Helvetica",16), command=self.button_save)
-        self.buttonSave.place(relx=0.40, rely=0.75)
+        self.buttonSave = Button(master, text="Guardar Partida", bg="blue", bd=5, justify=CENTER, width=15, font=("Helvetica",16), command=self.button_save)
+        self.buttonSave.place(relx=0.10, rely=0.40)
+
+        self.buttonLoad = Button(master, text="Salir", bg="red", bd=5, justify=CENTER, width=10, font=("Helvetica", 16), command=self.button_quit)
+        self.buttonLoad.place(relx=0.40, rely=0.80)
 
     # Subroutine that extracts the players names and stores them into the class attributes
     def button_load(self):
-
+        pass
 
     def button_save(self):
-
+        pass
 
     # Subroutine that closes the complete window
     def button_quit(self):
@@ -33,7 +36,7 @@ class VentanaGuardar(Frame):
 def main():
     root = Tk()
     root.geometry("600x300")
-    app = VentanaInicial(root)#.pack()
+    app = VentanaGuardar(root)#.pack()
     root.mainloop()
 
 if __name__ == '__main__':
