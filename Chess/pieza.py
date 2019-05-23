1cb4fd6392208e0619b25f453455f2f338c04dc2
Josué Zeledón <josue.zeledon0727@gmail.com>
2019-05-23 11:36:26 -0600

    Pieces classes added

---
+++ Chess/pieza.py
@@ -0,0 +1,34 @@
+from abc import ABCMeta, abstractmethod
+
+# Clase pieza de la cual heredaran las demás piezas del ajedrez
+# Tiene un color, una coordenadaX, una coordenadaY
+class Pieza(metaclass=ABCMeta):
+	
+	color = ''
+	coordX = 0
+	coordY = 0
+	isPlaying = True
+		
+	def getColor(self):
+		return self.color
+		
+	def getCoordX(self):
+		return self.coordX
+	
+	def getCoordY(self):
+		return self.coordY
+		
+	def setCoordX(self, nuevaCoordX):
+		self.coordX = nuevaCoordX
+	
+	def setCoordY(self, nuevaCoordY):
+		self.coordY = nuevaCoordY
+
+	@abstractmethod
+	def canMove(self):
+		pass
+		
+		
+##piece = Pieza('b', 1, 6)
+
+##print("Color es: "+ piece.getColor() + " y coordenadas(x,y) son: " + str(piece.getCoordX()) + " " + str(piece.getCoordY()))
---
+++ Chess/reina.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from pieza import Pieza
+
+class Reina(Pieza):
+	
+	def __init__(self, color, coordX, coordY, imagen_archivo):
+		self.color = color
+		self.coordX = coordX
+		self.coordY = coordY
+		self.image = PhotoImage(file=imagen_archivo)
+        
+	def printCoords(self):
+		print("Color es: "+ self.getColor() + " y coordenadas(x,y) son: " + str(self.getCoordX()) + " " + str(self.getCoordY()))
+		
+	def canMove(self):
+		pass
