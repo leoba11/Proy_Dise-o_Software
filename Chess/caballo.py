d273128c583774e02ed04b0bb8b47deca1e87b8f
Josué Zeledón <josue.zeledon0727@gmail.com>
2019-05-28 15:23:48 -0600

    knight movement added

--- Chess/caballo.py
+++ Chess/caballo.py
@@ -12,5 +12,36 @@ class Caballo(Pieza):
 	def printCoords(self):
 		print("Color es: "+ self.getColor() + " y coordenadas(x,y) son: " + str(self.getCoordX()) + " " + str(self.getCoordY()))
 		
-	def canMove(self):
-		pass
+	# Function that returns the tuples with the possible moves of the piece		
+	def canMove(self, chessBoard):
+		
+		actual_coordX = self.getCoordX()
+		actual_coordY = self.getCoordY()
+		
+		# Creates an empty list
+		possible_moves = []
+		
+		# Sets the possible coordenates of the movements for the knight
+		coordsX = [-1, -1, -2, 2, 1, 1, -2, 2]
+		coordsY = [-2, 2, -1, 1, -2, 2, 1, 1]
+		
+		for index in range(7):
+			
+			if ( chessBoard.isEmpty(actual_coordX + coordsX[index], actual_coordY + coordsY[index]) ):
+				possible_moves.append( (actual_coordX + coordsX[index], actual_coordY + coordsY[index]) )
+			
+			else:
+				if ( chessBoard.isEnemy(self.getColor(), actual_coordX + coordsX[index], actual_coordY + coordsY[index]) ):
+					possible_moves.append( (actual_coordX + coordsX[index], actual_coordY + coordsY[index]) )
+					
+		return possible_moves
+		
+	# Makes the knight movement
+	def movePiece(self, newCoordinates):
+		
+		self.setCoordX(newCoordinates[0])
+		self.setCoordY(newCoordinates[1])
+		
+root = Tk()
+caballito = Caballo('b', 0, 6, "images/bn.gif")
+caballito.printCoords()
