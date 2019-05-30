d76f047265d01c25036d8ca1821e6ab9fcd8700a
Luis Carvajal <luisda7cr@gmail.com>
2019-05-30 16:22:21 -0600

    load pieces done

--- Chess/logicChessBoard.py
+++ Chess/logicChessBoard.py
@@ -7,6 +7,8 @@ from torre import *
 from chessBoard import *
 from tkinter import *
 
+piecesforLogic = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
 class logicChessBoard():
 
     def __init__(self, rows = 8, cols = 8):
@@ -23,120 +25,121 @@ class logicChessBoard():
         return board
 
     def loadPieces(self):
-        piecesforLogic = []
         
+        root = Tk()
         path = os.getcwd()
         defPath = path + "/images/"
         imgs = os.listdir(defPath)
+        global piecesforLogic
 
-        self.black_king = Rey('b', 0, 4, defPath + "bk.gif")
+        #--------------------Black Empire-----------------------------
+        black_king = Rey('b', 0, 4, defPath + "bk.gif")
         piecesforLogic[0] = (black_king, (0,4))
 
-        self.black_queen = Reina('b', 0,3, defPath + "bq.gif")
+        black_queen = Reina('b', 0,3, defPath + "bq.gif")
         piecesforLogic[1] = (black_queen, (0,3))
 
-
-        return self.piecesforLogic
-        '''
         black_bishop1 = Alfil('b', 0, 2, defPath + "bb.gif")
-        black_bishop1.image.image = black_bishop1.image
-        self.addPiece("bbr", black_bishop1.image, black_bishop1.getCoordX(), black_bishop1.getCoordY())
-        black_bishop2 = Alfil('b', 0, 5, defPath + "bb.gif")
-        black_bishop2.image.image = black_bishop2.image
-        self.addPiece("bbl", black_bishop2.image, black_bishop2.getCoordX(), black_bishop2.getCoordY())
-        black_knigth1 = Caballo('b', 0, 1, defPath + "bn.gif")
-        black_knigth1.image.image = black_knigth1.image
-        self.addPiece("bnr", black_knigth1.image, black_knigth1.getCoordX(), black_knigth1.getCoordY())
-        black_knigth2 = Caballo('b', 0, 6, defPath + "bn.gif")
-        black_knigth2.image.image = black_knigth2.image
-        self.addPiece("bnl", black_knigth2.image, black_knigth2.getCoordX(), black_knigth2.getCoordY())
-        black_rook1 = Torre('b', 0, 0, defPath + "br.gif")
-        black_rook1.image.image = black_rook1.image
-        self.addPiece("brr", black_rook1.image, black_rook1.getCoordX(), black_rook1.getCoordY())
-        black_rook2 = Torre('b', 0, 7, defPath + "br.gif")
-        black_rook2.image.image = black_rook2.image
-        self.addPiece("brl", black_rook2.image, black_rook2.getCoordX(), black_rook2.getCoordY())
-        black_pawn1 = Peon('b', 1, 0, defPath + "bp.gif")
-        black_pawn1.image.image = black_pawn1.image
-        self.addPiece("bp0", black_pawn1.image, black_pawn1.getCoordX(), black_pawn1.getCoordY())
-        black_pawn2 = Peon('b', 1, 1, defPath + "bp.gif")
-        black_pawn2.image.image = black_pawn2.image
-        self.addPiece("bp1", black_pawn2.image, black_pawn2.getCoordX(), black_pawn2.getCoordY())
-        black_pawn3 = Peon('b', 1, 2, defPath + "bp.gif")
-        black_pawn3.image.image = black_pawn3.image
-        self.addPiece("bp2", black_pawn3.image, black_pawn3.getCoordX(), black_pawn3.getCoordY())
-        black_pawn4 = Peon('b', 1, 3, defPath + "bp.gif")
-        black_pawn4.image.image = black_pawn4.image
-        self.addPiece("bp3", black_pawn4.image, black_pawn4.getCoordX(), black_pawn4.getCoordY())
-        black_pawn5 = Peon('b', 1, 4, defPath + "bp.gif")
-        black_pawn5.image.image = black_pawn5.image
-        self.addPiece("bp4", black_pawn5.image, black_pawn5.getCoordX(), black_pawn5.getCoordY())
-        black_pawn6 = Peon('b', 1, 5, defPath + "bp.gif")
-        black_pawn6.image.image = black_pawn6.image
-        self.addPiece("bp5", black_pawn6.image, black_pawn6.getCoordX(), black_pawn6.getCoordY())
-        black_pawn7 = Peon('b', 1, 6, defPath + "bp.gif")
-        black_pawn7.image.image = black_pawn7.image
-        self.addPiece("bp6", black_pawn7.image, black_pawn7.getCoordX(), black_pawn7.getCoordY())
-        black_pawn8 = Peon('b', 1, 7, defPath + "bp.gif")
-        black_pawn8.image.image = black_pawn8.image
-        self.addPiece("bp7", black_pawn8.image, black_pawn8.getCoordX(), black_pawn8.getCoordY())
-        
-        
-        # Creates all the white pieces and draws them into the board
-        white_king = Rey('w', 7, 4, defPath + "wk.gif")
-        white_king.image.image = white_king.image
-        self.addPiece("wk", white_king.image, white_king.getCoordX(), white_king.getCoordY())
-        white_queen = Reina('w', 7,3, defPath + "wq.gif")
-        white_queen.image.image = white_queen.image
-        self.addPiece("wq", white_queen.image, white_queen.getCoordX(), white_queen.getCoordY())
-        white_bishop1 = Alfil('w', 7, 2, defPath + "wb.gif")
-        white_bishop1.image.image = white_bishop1.image
-        self.addPiece("wbl", white_bishop1.image, white_bishop1.getCoordX(), white_bishop1.getCoordY())
-        white_bishop2 = Alfil('w', 7, 5, defPath + "wb.gif")
-        white_bishop2.image.image = white_bishop2.image
-        self.addPiece("wbr", white_bishop2.image, white_bishop2.getCoordX(), white_bishop2.getCoordY())
-        white_knigth1 = Caballo('w', 7, 1, defPath + "wn.gif")
-        white_knigth1.image.image = white_knigth1.image
-        self.addPiece("wnl", white_knigth1.image, white_knigth1.getCoordX(), white_knigth1.getCoordY())
-        white_knigth2 = Caballo('w', 7, 6, defPath + "wn.gif")
-        white_knigth2.image.image = white_knigth2.image
-        self.addPiece("wnr", white_knigth2.image, white_knigth2.getCoordX(), white_knigth2.getCoordY())
-        white_rook1 = Torre('w', 7, 0, defPath + "wr.gif")
-        white_rook1.image.image = white_rook1.image
-        self.addPiece("wrl", white_rook1.image, white_rook1.getCoordX(), white_rook1.getCoordY())
-        white_rook2 = Torre('w', 7, 7, defPath + "wr.gif")
-        white_rook2.image.image = white_rook2.image
-        self.addPiece("wrr", white_rook2.image, white_rook2.getCoordX(), white_rook2.getCoordY())
-        white_pawn1 = Peon('w', 6, 0, defPath + "wp.gif")
-        white_pawn1.image.image = white_pawn1.image
-        self.addPiece("wp0", white_pawn1.image, white_pawn1.getCoordX(), white_pawn1.getCoordY())
-        white_pawn2 = Peon('w', 6, 1, defPath + "wp.gif")
-        white_pawn2.image.image = white_pawn2.image
-        self.addPiece("wp1", white_pawn2.image, white_pawn2.getCoordX(), white_pawn2.getCoordY())
-        white_pawn3 = Peon('w', 6, 2, defPath + "wp.gif")
-        white_pawn3.image.image = white_pawn3.image
-        self.addPiece("wp2", white_pawn3.image, white_pawn3.getCoordX(), white_pawn3.getCoordY())
-        white_pawn4 = Peon('w', 6, 3, defPath + "wp.gif")
-        white_pawn4.image.image = white_pawn4.image
-        self.addPiece("wp3", white_pawn4.image, white_pawn4.getCoordX(), white_pawn4.getCoordY())
-        white_pawn5 = Peon('w', 6, 4, defPath + "wp.gif")
-        white_pawn5.image.image = white_pawn5.image
-        self.addPiece("wp4", white_pawn5.image, white_pawn5.getCoordX(), white_pawn5.getCoordY())
-        white_pawn6 = Peon('w', 6, 5, defPath + "wp.gif")
-        white_pawn6.image.image = white_pawn6.image
-        self.addPiece("wp5", white_pawn6.image, white_pawn6.getCoordX(), white_pawn6.getCoordY())
-        white_pawn7 = Peon('w', 6, 6, defPath + "wp.gif")
-        white_pawn7.image.image = white_pawn7.image
-        self.addPiece("wp6", white_pawn7.image, white_pawn7.getCoordX(), white_pawn7.getCoordY())
-        white_pawn8 = Peon('w', 6, 7, defPath + "wp.gif")
-        white_pawn8.image.image = white_pawn8.image
-        self.addPiece("wp7", white_pawn8.image, white_pawn8.getCoordX(), white_pawn8.getCoordY())
-        '''
-    
+        piecesforLogic[2] = (black_bishop1,(0,2))
 
+        black_bishop2 = Alfil('b', 0, 5, defPath + "bb.gif")
+        piecesforLogic[3] = (black_bishop2, (0,5))
+
+        black_knigth1 = Caballo('b', 0, 1, defPath + "bn.gif")
+        piecesforLogic[4] = (black_knigth1, (0,1))
+
+        black_knigth2 = Caballo('b', 0, 6, defPath + "bn.gif")
+        piecesforLogic[5] = (black_knigth2, (0,6))
+
+        black_rook1 = Torre('b', 0, 0, defPath + "br.gif")
+        piecesforLogic[6] = (black_rook1, (0,0))
+
+        black_rook2 = Torre('b', 0, 7, defPath + "br.gif")
+        piecesforLogic[7] = (black_rook2, (0,7))
+
+        black_pawn1 = Peon('b', 1, 0, defPath + "bp.gif")
+        piecesforLogic[8] = (black_pawn1, (1,0))
+
+        black_pawn2 = Peon('b', 1, 1, defPath + "bp.gif")
+        piecesforLogic[9] = (black_pawn1, (1,1))
+
+        black_pawn3 = Peon('b', 1, 2, defPath + "bp.gif")
+        piecesforLogic[10] = (black_pawn1, (1,2))
+
+        black_pawn4 = Peon('b', 1, 3, defPath + "bp.gif")
+        piecesforLogic[11] = (black_pawn1, (1,3))
+
+        black_pawn5 = Peon('b', 1, 4, defPath + "bp.gif")
+        piecesforLogic[12] = (black_pawn1, (1,4))
+
+        black_pawn6 = Peon('b', 1, 5, defPath + "bp.gif")
+        piecesforLogic[13] = (black_pawn1, (1,5))
+
+        black_pawn7 = Peon('b', 1, 6, defPath + "bp.gif")
+        piecesforLogic[14] = (black_pawn1, (1,6))
+
+        black_pawn8 = Peon('b', 1, 7, defPath + "bp.gif")
+        piecesforLogic[15] = (black_pawn1, (1,7))
+        #--------------------Black Empire-----------------------------
+        #--------------------White Empire-----------------------------
+        white_king = Rey('w', 7, 4, defPath + "wk.gif")
+        piecesforLogic[16] = (white_king, (7,4))
+
+        white_queen = Reina('w', 7,3, defPath + "wq.gif")
+        piecesforLogic[17] = (white_queen, (7,3))
+
+        white_bishop1 = Alfil('w', 7, 2, defPath + "wb.gif")
+        piecesforLogic[18] = (white_bishop1, (7,2))
+
+        white_bishop2 = Alfil('w', 7, 5, defPath + "wb.gif") 
+        piecesforLogic[19] = (white_bishop2, (7,5))
+
+        white_knigth1 = Caballo('w', 7, 1, defPath + "wn.gif")
+        piecesforLogic[20] = (white_knigth1, (7,1))
+
+        white_knigth2 = Caballo('w', 7, 6, defPath + "wn.gif")
+        piecesforLogic[21] = (white_knigth2, (7,6))
+
+        white_rook1 = Torre('w', 7, 0, defPath + "wr.gif")
+        piecesforLogic[22] = (white_rook1, (7,0))
+
+        white_rook2 = Torre('w', 7, 7, defPath + "wr.gif")
+        piecesforLogic[23] =  (white_rook2, (7,7))
+
+        white_pawn1 = Peon('w', 6, 0, defPath + "wp.gif")
+        piecesforLogic[24] = (white_pawn1, (6,0))
+
+        white_pawn2 = Peon('w', 6, 1, defPath + "wp.gif")
+        piecesforLogic[25] = (white_pawn1, (6,1))        
+
+        white_pawn3 = Peon('w', 6, 2, defPath + "wp.gif")
+        piecesforLogic[26] = (white_pawn1, (6,2))
+
+        white_pawn4 = Peon('w', 6, 3, defPath + "wp.gif")
+        piecesforLogic[27] = (white_pawn1, (6,3))
+
+        white_pawn5 = Peon('w', 6, 4, defPath + "wp.gif")
+        piecesforLogic[28] = (white_pawn1, (6,4))
+
+        white_pawn6 = Peon('w', 6, 5, defPath + "wp.gif")
+        piecesforLogic[29] = (white_pawn1, (6,5))
+
+        white_pawn7 = Peon('w', 6, 6, defPath + "wp.gif")
+        piecesforLogic[30] = (white_pawn1, (6,6))
+
+        white_pawn8 = Peon('w', 6, 7, defPath + "wp.gif")
+        piecesforLogic[31] = (white_pawn1, (6,7))
+        #--------------------White Empire-----------------------------
+        return piecesforLogic
+
+    def prueba(self):
+        global piecesforLogic
+        print (piecesforLogic[0][1][0])
 
 if __name__ == '__main__':
     board = logicChessBoard()
     print(board.drawBoard())
     print(board.loadPieces())
+    print("=====================================")
+    board.prueba()
 
