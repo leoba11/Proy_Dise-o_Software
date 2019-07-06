064dfadf5a53a49120ac03b04584f8b7a2c5cf98
Luis Carvajal <luisda7cr@gmail.com>
2019-07-06 17:24:40 -0600

    Documentación generada, pero solo de las abs y gen

--- Chess/jugadorAbs.py
+++ Chess/jugadorAbs.py
@@ -1,7 +1,13 @@
 from abc import ABCMeta, abstractmethod
 
 class JugadorAbs(metaclass=ABCMeta):
-    
+    """
+    Clase Jugador Abstracto:
+        Es el jugador que actualmente esta en su turno.
+    """
     @abstractmethod
     def getName(self):
+        """
+        :return: Nombre del jugador
+        """
         pass
